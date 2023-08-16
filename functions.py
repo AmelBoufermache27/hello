9b307785244e2b4bb093f3fb9ae68ed25fae807b
amel boufermache <amelboufermache@gmail.com>
2023-08-16 10:58:44 +0100

    add files python

---
+++ functions.py
@@ -0,0 +1,6 @@
+import squares 
+
+x=int(input("Number:"))
+
+for i in range (10):
+    print(f"square of : {x} is {squares.square(x)}")
---
+++ hellopython.py
@@ -0,0 +1,4 @@
+names=["amel","moatez","djihane","djana"]
+names.append("UK")
+names.sort()
+print(names)
